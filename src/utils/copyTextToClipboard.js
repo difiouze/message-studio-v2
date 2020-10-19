d8359400e83059b9800935b020dd391c96313031
laurent.ravix@oxatis.com <RDP6tm56Lenny13>
2020-10-19 17:11:13 +0200

    Final script copy + layout ok when paste

--- src/utils/copyTextToClipboard.js
+++ src/utils/copyTextToClipboard.js
@@ -1,25 +1,11 @@
 const copyTextToClipboard = () => {
 
-  let copiedText = document.getElementById('copiedText').innerText;
-  console.log(copiedText)
-  
-   // Create an auxiliary hidden input
-    var aux = document.createElement("input");
-  
-    // // Get the text from the element passed into the input
-    aux.setAttribute("value", copiedText);
-  
-    // // Append the aux input to the body
-    document.body.appendChild(aux);
-  
-    // // Highlight the content
-    aux.select();
-  
-    // // Execute the copy command
-    document.execCommand("copy");
-  
-    // // Remove the input from the body
-    document.body.removeChild(aux);
+  var r = document.createRange();
+  r.selectNode(document.getElementById('copiedText'));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
   
   }
 
